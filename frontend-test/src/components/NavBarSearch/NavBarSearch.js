decf48f05dd66dcf576503a57be5add138316fc5
tomasdelucaa <tomasdelucaa@gmail.com>
2022-11-12 10:33:43 -0300

    NavBar created and finished

--- frontend-test/src/components/NavBarSearch/NavBarSearch.js
+++ frontend-test/src/components/NavBarSearch/NavBarSearch.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import UserSearched from "../UserSearched/UserSearched";
+import { Link } from "react-router-dom";
+import "./NavBarSearch.css";
 
 const NavBarSearch = () => {
   const [input, setInput] = useState("");
@@ -21,26 +22,62 @@ const NavBarSearch = () => {
   };
   console.log(user);
   return (
-    <div>
-      <h1>GitHub user searcher</h1>
-      <input
-        value={input}
-        onChange={(e) => {
-          e.target.value != 0 && setInput(e.target.value);
-        }}
-      />
-      <button style={{ marginBottom: "20px" }} onClick={handleSearch}>
-        Buscar
-      </button>
-      {search && (
-        <div style={{ margin: "20px 0" }}>
-          <UserSearched {...user} />{" "}
-          <button onClick={() => setSearch(false)}>
-            Quitar usuario buscado
+    <div className="navBar">
+      <div className="navBarLogo">
+        <a href="https://www.github.com">
+          <img
+            src="https://cdn.icon-icons.com/icons2/2429/PNG/512/github_logo_icon_147285.png"
+            className="gitHubLogo"
+          />
+        </a>
+      </div>
+
+      <div className="navBarSearcher">
+        <div>
+          <input
+            value={input}
+            className="searchInput"
+            onChange={(e) => {
+              e.target.value != 0 && setInput(e.target.value);
+            }}
+            placeholder="Profile search"
+          />
+
+          <button
+            style={{ marginBottom: "20px" }}
+            onClick={handleSearch}
+            className="searchButton"
+          >
+            <Link to={`/userDetail/${input}`}>Find</Link>
           </button>
         </div>
-      )}
+      </div>
+
+      <div className="navBarLink">
+        <Link to={"/"}>Home page</Link>
+      </div>
     </div>
+
+    // <div>
+    //   <h1>GitHub user searcher</h1>
+    //   <input
+    //     value={input}
+    //     onChange={(e) => {
+    //       e.target.value != 0 && setInput(e.target.value);
+    //     }}
+    //   />
+    //   <button style={{ marginBottom: "20px" }} onClick={handleSearch}>
+    //     Buscar
+    //   </button>
+    //   {search && (
+    //     <div style={{ margin: "20px 0" }}>
+    //       <UserSearched {...user} />{" "}
+    //       <button onClick={() => setSearch(false)}>
+    //         Quitar usuario buscado
+    //       </button>
+    //     </div>
+    //   )}
+    // </div>
   );
 };
 
